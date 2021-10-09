fbba4a8f7a16b7d2ac148716da1d0fd7420fde35
sujang958 <enter7377@naver.com>
2021-10-09 17:18:07 +0900

    reaf: project setting

--- backend/main.go
+++ backend/main.go
@@ -11,6 +11,11 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		panic("couldnt import .env file")
 	}
+	// res, err := http.Get("https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGRqTVhZU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen")
+	// if err != nil {
+	// 	log.Fatalln("Failure getting news")
+	// }
+	// io.ReadAll() res.Body.Read()
 
 	mux := http.NewServeMux()
 	log.Fatalln(http.ListenAndServe(":3000", mux))
